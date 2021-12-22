dc2e5e0358b2f9acfb19bd1a25a26a0dbda48221
tinoschroeter <mail@wgnetworks.de>
2021-12-22 18:20:01 +0100

    fix: component

--- movie/pages/movie/[id].js
+++ movie/pages/movie/[id].js
@@ -1,11 +1,11 @@
 import { useRouter } from "next/router";
 import React, { useState, useEffect } from "react";
 
-const videoCard = () => {
+const VideoCard = () => {
   const router = useRouter();
   const { id } = router.query;
 
   return <p>Video: {id}</p>;
 };
 
-export default videoCard;
+export default VideoCard;
