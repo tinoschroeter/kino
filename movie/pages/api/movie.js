983f6bdfdcfc457d2651dd95f0c7b61b49bde62b
Tino Schroter <tino.schroeter@gmail.com>
2022-02-02 20:54:57 +0100

    update: update interval to 7 hours

--- movie/pages/api/movie.js
+++ movie/pages/api/movie.js
@@ -7,9 +7,9 @@ const handler = nextConnect();
 handler.use(middleware);
 handler.get(async (req, res) => {
   let time = Date.now();
-  time = time - 60 * 60 * 1000;
+  time = time - 60 * 60 * 7000;
 
-  const query = { updated: { $gt: time } };
+  const query = { updated: { $gt: time } }; // not older then 7 hours
   const options = {
     sort: { vote_average: -1 }, // ascending = 1, descending = -1
     projection: { _id: 0 }, // remove _id
